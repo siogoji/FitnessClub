e0bea6a41eb4d5ceb414146a18dc04f3adf4017d
Egor <stoliarchukk.egor@gmail.com>
2023-12-10 16:30:06 +0200

    Added localization

--- FitnessClub/Program.cs
+++ FitnessClub/Program.cs
@@ -1,9 +1,16 @@
 using FitnessClub.Models;
 using FitnessClub.Repositories;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+builder.Services.AddRazorPages()
+    .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+    .AddDataAnnotationsLocalization();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -23,6 +30,14 @@ builder.Services.AddScoped<IUserAuthenticationService, UserAuthenticationService
 
 var app = builder.Build();
 
+var supportedCultures = new[] { "uk", "en" };
+var localizationOptions = new RequestLocalizationOptions()
+    .SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+
+app.UseRequestLocalization(localizationOptions);
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -32,13 +47,18 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseMiddleware<LanguageMiddleware>();
+
 app.UseAuthentication();
 
 app.UseAuthorization();
+
+app.MapRazorPages();
 
 app.MapControllerRoute(
     name: "default",
